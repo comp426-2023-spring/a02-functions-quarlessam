c56dadbab2769ab7772f2d397c3b297789f0bc02
quarlessam <quarlessam@gmail.com>
2023-04-10 23:43:20 -0400

    first

---
+++ cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+import minimist from 'minimist'
+import moment from 'moment-timezone'
+import fetch from 'node-fetch'
+
+const args = minimist(process.argv.slice(2))
+
+console.log(args)
+
+if(args.h) {
+  let helptext = `Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+    -h            Show this help message and exit.
+    -n, -s        Latitude: N positive; S negative.
+    -e, -w        Longitude: E positive; W negative.
+    -z            Time zone: uses tz.guess() from moment-timezone by default.
+    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+    -j            Echo pretty JSON from open-meteo API and exit.`
+  console.log(helptext)
+  process.exit()
+}
+
+const timezone = moment.tz.guess()
+console.log(timezone)
+const latitude = args.n || (-1 * args.s)
+const longitude = args.e || (-1 * args.w)
+
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&daily=weathercode,precipitation_hours&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=' + timezone)
+const data = await response.json()
+
+if (args.j) {
+  process.stdout.write(data + "\n")
+  process.exit()
+}
